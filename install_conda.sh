18211ea402f89dba7a9dfd71c3bc74054e2fac80
Abdelmajid EL HOU <abdelmajid.elhou@gmail.com>
2023-08-15 17:40:12 +0200

    add conda

---
+++ install_conda.sh
@@ -0,0 +1,7 @@
+# Téléchargement de Miniconda (pour Python 3)
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+# Installation
+bash Miniconda3-latest-Linux-x86_64.sh
+
+# Suivez les instructions à l'écran pour l'installation
